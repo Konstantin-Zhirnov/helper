e8281ef550f56ab8f1daa71d6059a37ffcb90bd1
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2023-10-30 09:37:49 -0700

    Fix bag for docker

--- client/src/widgets/ProfileInfo/index.tsx
+++ client/src/widgets/ProfileInfo/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import {
-  Box,
-  Card,
-  CardBody,
-  CardHeader,
-  Heading,
-  Text,
-} from '@chakra-ui/react'
-import { FaTelegram, FaSquareWhatsapp, FaViber } from 'react-icons/fa6'
+import { Box, Card, CardBody, CardHeader, Heading, Text } from '@chakra-ui/react'
+import { FaSquareWhatsapp, FaTelegram, FaViber } from 'react-icons/fa6'
 
 import { useAppSelector } from '../../app'
-import { getUser, EditablePasswordInput, EditableInput } from '../../features'
-
+import { EditableInput, EditablePasswordInput, getUser } from '../../features'
 
 import classes from './ProfileInfo.module.sass'
-
 
 
 const ProfileInfo: React.FC = () => {
@@ -28,7 +19,7 @@ const ProfileInfo: React.FC = () => {
         </CardHeader>
         <CardBody>
           <Box>
-            <EditableInput _id={user._id} defaultValue={user.name} field="name"/>
+            <EditableInput _id={user._id} defaultValue={user.name} field='name' />
           </Box>
         </CardBody>
       </Card>
@@ -50,7 +41,7 @@ const ProfileInfo: React.FC = () => {
         </CardHeader>
         <CardBody>
           <Box>
-            <EditablePasswordInput _id={user._id}/>
+            <EditablePasswordInput _id={user._id} />
           </Box>
         </CardBody>
       </Card>
@@ -61,40 +52,41 @@ const ProfileInfo: React.FC = () => {
         </CardHeader>
         <CardBody>
           <Box>
-            <EditableInput _id={user._id} defaultValue={user.phone} field="phone"/>
+            <EditableInput _id={user._id} defaultValue={user.phone} field='phone' />
           </Box>
         </CardBody>
       </Card>
 
       <Card className={classes.card}>
         <CardHeader className={classes.cardHeader}>
-          <Heading size='md' className={classes.heading}><FaSquareWhatsapp className={classes.whatsapp}/>WhatsApp:</Heading>
+          <Heading size='md' className={classes.heading}><FaSquareWhatsapp
+            className={classes.whatsapp} />WhatsApp:</Heading>
         </CardHeader>
         <CardBody>
           <Box>
-            <EditableInput _id={user._id} defaultValue={user.whatsapp} field="whatsapp"/>
+            <EditableInput _id={user._id} defaultValue={user.whatsapp} field='whatsapp' />
           </Box>
         </CardBody>
       </Card>
 
       <Card className={classes.card}>
         <CardHeader className={classes.cardHeader}>
-          <Heading size='md' className={classes.heading}><FaTelegram className={classes.telegram}/> Telegram:</Heading>
+          <Heading size='md' className={classes.heading}><FaTelegram className={classes.telegram} /> Telegram:</Heading>
         </CardHeader>
         <CardBody>
           <Box>
-            <EditableInput _id={user._id} defaultValue={user.telegram} field="telegram"/>
+            <EditableInput _id={user._id} defaultValue={user.telegram} field='telegram' />
           </Box>
         </CardBody>
       </Card>
 
       <Card className={classes.card}>
         <CardHeader className={classes.cardHeader}>
-          <Heading size='md' className={classes.heading}><FaViber className={classes.viber}/>Viber:</Heading>
+          <Heading size='md' className={classes.heading}><FaViber className={classes.viber} />Viber:</Heading>
         </CardHeader>
         <CardBody>
           <Box>
-            <EditableInput _id={user._id} defaultValue={user.viber} field="viber"/>
+            <EditableInput _id={user._id} defaultValue={user.viber} field='viber' />
           </Box>
         </CardBody>
       </Card>
