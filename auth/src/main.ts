ae37ea4d21c64cd73faddfb45847e94ce3f2263f
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2023-10-30 10:46:18 -0700

    SSl

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,31 +1,39 @@
-import { NestFactory } from '@nestjs/core';
-import * as cookieParser from "cookie-parser";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'
 
 
 async function bootstrap() {
+  const fs = require('fs')
+  const keyFile = fs.readFileSync(__dirname + '/ssl/privkey.pem')
+  const certFile = fs.readFileSync(__dirname + '/ssl/fullchain.pem')
 
   const PORT = process.env.PORT
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile,
+    },
+  })
   app.setGlobalPrefix('api')
 
   const config = new DocumentBuilder()
-    .setTitle("Authentication")
-    .setDescription("REST API documentation")
-    .setVersion("1.0.0")
+    .setTitle('Authentication')
+    .setDescription('REST API documentation')
+    .setVersion('1.0.0')
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('/docs', app, document)
 
   app.use(cookieParser())
   app.enableCors({
-      credentials: true,
-      origin: true,
-      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"]
+    credentials: true,
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
   })
-    await app.listen(PORT || 5000)
-  }
+  await app.listen(PORT || 5000)
+}
 
 bootstrap()
